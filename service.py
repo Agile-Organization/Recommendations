0c59a8fcf7ae35e6d952654c7271ed6c0b28758b
Eliellel <15117485+lj894@users.noreply.github.com>
2020-10-12 16:43:11 -0400

    Update service.py

--- service.py
+++ service.py
@@ -120,5 +120,3 @@ def check_content_type(content_type):
         return
     app.logger.error("Invalid Content-Type: %s", request.headers["Content-Type"])
     abort(415, "Content-Type must be {}".format(content_type))
-
-    
