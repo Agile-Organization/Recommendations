b37e96bed9b8c36cc411551c96b89f4c1c8fe72a
Sushanth Samala <ss12852@nyu.edu>
2020-10-17 17:31:52 -0400

    cleanup

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,7 @@
-""" Recommendation Project Test Module
-
-This module contains classes to test Recommendation model
-and Recommendation Service throughly
-
+"""
+Project: Recommendations test
+This package contains classes to test Recommendation model
+and Recommendation Service
 """
 from tests.test_recommendation import TestRecommendation
 from tests.test_service import TestRecommendationService
