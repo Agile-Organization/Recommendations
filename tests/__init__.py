82a1a4e20135fc52b3f331f1a2331724e13702bd
Sushanth Samala <ss12852@nyu.edu>
2020-10-25 13:21:53 -0400

    Added Travis CI and Code Cov

--- tests/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Project: Recommendations test
-This package contains classes to test Recommendation model
-and Recommendation Service
-"""
-from tests.test_recommendation import TestRecommendation
-from tests.test_service import TestRecommendationService
