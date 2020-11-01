a9eb19a3efe23c041c24546877faa3e866cb6310
Minerva <minervamcheng@gmail.com>
2020-11-01 01:02:42 -0400

    set up getting db credentials when in cloud

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 import logging
 
@@ -6,6 +7,11 @@ import logging
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgres://postgres:postgres@localhost:5432/postgres"
 )
+
+# Override if we are running in Cloud Foundry
+if 'VCAP_SERVICES' in os.environ:
+    vcap = json.loads(os.environ['VCAP_SERVICES'])
+    DATABASE_URI = vcap['user-provided'][0]['credentials']['url']
 
 # Configure SQLAlchemy
 SQLALCHEMY_DATABASE_URI = DATABASE_URI
