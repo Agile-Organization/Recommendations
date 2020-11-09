7e0b82e0ed0e72206b643878119964ffcae6d052
Sushanth Samala <ss12852@nyu.edu>
2020-11-08 23:30:45 -0500

    Added home page test (#112)
    * Added home page test
    
    * added create recommendations

--- features/recommendations.feature
+++ features/recommendations.feature
@@ -3,6 +3,16 @@ Feature: The recommendation service back-end
     I need a RESTful catalog service
     So that I can keep track of all the recommendations
 
+Background:
+    Given the following recommendations
+        | product-id       | related-product-id | type-id | status |
+        | 10               | 22                 | 2       | True   |
+
+Scenario: The server is running
+    When I visit the "Home Page"
+    Then I should see "Recommendation RESTful Service" in the title
+    And I should not see "404 Not Found"
+
 Scenario: Create a Recommendation
     When I visit the "Home Page"
     And I set the "product_id" to "21"
--- features/steps/recommendation_steps.py
+++ features/steps/recommendation_steps.py
@@ -13,10 +13,44 @@ from selenium.webdriver.support import expected_conditions
 WAIT_SECONDS = int(getenv('WAIT_SECONDS', '60'))
 ID_PREFIX = 'recommendation_'
 
+@given('the following recommendations')
+def step_impl(context):
+    """ Delete all Recommendations and load new ones """
+    headers = {'Content-Type': 'application/json'}
+    # list all of the recommendations and delete them one by one
+    context.resp = requests.get(context.base_url + '/recommendations', headers=headers)
+    expect(context.resp.status_code).to_equal(200)
+    for recommendation in context.resp.json():
+        context.resp = requests.delete(context.base_url + '/recommendations/' + str(recommendation["product-id"]) + "/" + str(recommendation["related-product-id"]), headers=headers)
+        expect(context.resp.status_code).to_equal(204)
+    
+    # load the database with new recommendations
+    create_url = context.base_url + '/recommendations'
+    for row in context.table:
+        data = {
+            "product-id": int(row['product-id']),
+            "related-product-id": int(row['related-product-id']),
+            "type-id": int(row['type-id']),
+            "status": row['status'] == "True"
+            }
+        payload = json.dumps(data)
+        context.resp = requests.post(create_url, data=payload, headers=headers)
+        expect(context.resp.status_code).to_equal(201)
+
 @when('I visit the "home page"')
 def step_impl(context):
     """ Make a call to the base URL """
     context.driver.get(context.base_url)
+
+@then('I should see "{message}" in the title')
+def step_impl(context, message):
+    """ Check the document title for a message """
+    expect(context.driver.title).to_contain(message)
+
+@then('I should not see "{message}"')
+def step_impl(context, message):
+    error_msg = "I should not see '%s' in '%s'" % (message, context.resp.text)
+    ensure(message in context.resp.text, False, error_msg)
 
 @when('I set the "{element_name}" to "{text_string}"')
 def step_impl(context, element_name, text_string):
