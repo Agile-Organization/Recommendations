22ad7716d170383804a25851c8c299bb49dd0b4a
Sushanth Samala <ss12852@nyu.edu>
2020-11-08 22:09:50 -0500

    added create recommendations

--- features/steps/recommendation_steps.py
+++ features/steps/recommendation_steps.py
@@ -13,6 +13,30 @@ from selenium.webdriver.support import expected_conditions
 WAIT_SECONDS = int(getenv('WAIT_SECONDS', '60'))
 ID_PREFIX = 'recommendation_'
 
+@given('the following recommendations')
+def step_impl(context):
+    """ Delete all Recommendations and load new ones """
+    headers = {'Content-Type': 'application/json'}
+    # list all of the recommendations and delete them one by one
+    context.resp = requests.get(context.base_url + '/recommendations', headers=headers)
+    expect(context.resp.status_code).to_equal(200)
+    for recommendation in context.resp.json():
+        context.resp = requests.delete(context.base_url + '/recommendations/' + str(recommendation["product-id"]) + "/" + str(recommendation["related-product-id"]), headers=headers)
+        expect(context.resp.status_code).to_equal(204)
+    
+    # load the database with new recommendations
+    create_url = context.base_url + '/recommendations'
+    for row in context.table:
+        data = {
+            "product-id": row['product-id'],
+            "related-product-id": row['related-product-id'],
+            "type-id": row['type-id'],
+            "status": row['status']
+            }
+        payload = json.dumps(data)
+        context.resp = requests.post(create_url, data=payload, headers=headers)
+        expect(context.resp.status_code).to_equal(201)
+
 @when('I visit the "home page"')
 def step_impl(context):
     """ Make a call to the base URL """
