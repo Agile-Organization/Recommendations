8d6624506be777c9b8cdee44734bbe5a4051618b
Mu-xsan <xumx07@gmail.com>
2020-11-08 21:42:13 -0500

    create a scenario for create operation in recommendations.feature (#109)
    * create a scenario for create operation in recommendations.feature
    * Add behave in .travis.yml to automate bdd testing
    * Fix incompatibility between chrome and chrome driver

---
+++ features/recommendations.feature
@@ -0,0 +1,13 @@
+Feature: The recommendation service back-end
+    As a recommendation service
+    I need a RESTful catalog service
+    So that I can keep track of all the recommendations
+
+Scenario: Create a Recommendation
+    When I visit the "Home Page"
+    And I set the "product_id" to "21"
+    And I set the "related_product_id" to "23"
+    And I set the "type_id" to "1"
+    And I select "True" in the "status" dropdown
+    And I press the "Create" button
+    Then I should see the message "Success"
---
+++ features/steps/recommendation_steps.py
@@ -0,0 +1,47 @@
+from os import getenv
+import logging
+import json
+import requests
+from behave import *
+from service import app
+from compare import expect, ensure
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditions
+
+WAIT_SECONDS = int(getenv('WAIT_SECONDS', '60'))
+ID_PREFIX = 'recommendation_'
+
+@when('I visit the "home page"')
+def step_impl(context):
+    """ Make a call to the base URL """
+    context.driver.get(context.base_url)
+
+@when('I set the "{element_name}" to "{text_string}"')
+def step_impl(context, element_name, text_string):
+    element_id = ID_PREFIX + element_name.lower()
+    element = context.driver.find_element_by_id(element_id)
+    element.clear()
+    element.send_keys(text_string)
+
+@when('I select "{text}" in the "{element_name}" dropdown')
+def step_impl(context, text, element_name):
+    element_id = ID_PREFIX + element_name.lower()
+    element = Select(context.driver.find_element_by_id(element_id))
+    element.select_by_visible_text(text)
+
+@when('I press the "{button}" button')
+def step_impl(context, button):
+    button_id = button.lower() + '-btn'
+    context.driver.find_element_by_id(button_id).click()
+
+@then('I should see the message "{message}"')
+def step_impl(context, message):
+    found = WebDriverWait(context.driver, WAIT_SECONDS).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'flash_message'),
+            message
+        )
+    )
+    expect(found).to_be(True)
