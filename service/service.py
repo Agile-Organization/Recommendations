2319a2687cd2973db0489335abaf55d2a0e28532
Minerva <32100098+MinervaMint@users.noreply.github.com>
2020-11-17 18:46:53 -0500

    Refactor put (#134)
    * refactor update to return updated recommendation

--- service/service.py
+++ service/service.py
@@ -446,7 +446,9 @@ def update_recommendation(product_id, related_product_id):
                     "related product %s from %s to %s.", recommendation.product_id,
                     recommendation.related_product_id, old_typeid, recommendation.type_id)
 
-    return '', status.HTTP_200_OK
+    message = old_recommendation.serialize()
+
+    return make_response(jsonify(message), status.HTTP_200_OK)
 
 
 ######################################################################
